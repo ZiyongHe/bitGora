2cd707d42120ab6c7747371a85ff905fb229f19e
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 04:30:13 -0500

    Remove route
    Unused

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { UserProvider } from './utils/UserContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import PublicRoute from './components/PublicRoute'
