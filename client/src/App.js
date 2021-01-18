7f75386a6959c5098f95741b6e62f0270fb0135d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 23:12:55 -0500

    Add state for authenticated user

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 
 function App() {
+  const [auth, setAuth] = useState({})
+
+  useEffect(() => {
+    fetch('/user/current-session')
+      .then((response) => response.json())
+      .then((session) => setAuth(session.data))
+  }, [])
+
   return (
     <Router>
       <Switch>
