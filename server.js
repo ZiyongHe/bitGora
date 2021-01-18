fc59e4b14d9516b4411330982a86d51af6025a75
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 19:48:12 -0500

    Set up server to use sessions, passport and flash

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('./config/passport')
+const flash = require('connect-flash')
 const mongoose = require('mongoose')
 const path = require('path')
 const PORT = process.env.PORT || 3001
@@ -27,6 +30,18 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
+
+// For sessions
+app.use(
+  session({
+    secret: 'fdhs397y2y539n',
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 // Define API routes here
 app.use('/user', require('./routes/user-routes.js'))
