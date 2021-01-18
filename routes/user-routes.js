e9e737bc4ea451242811734d7ee6c5d7f9b7bd07
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 19:54:13 -0500

    Rename error in failed login obj to err

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -21,7 +21,7 @@ router.post(
 // Failure route to deliver error message
 router.get('/login', (req, res) => {
   res.status(401).json({
-    error: req.flash('error'),
+    err: req.flash('error'),
   })
 })
 
