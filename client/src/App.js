0ec5744915a7a1d2cb0eff377eee34743b395c27
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 23:58:32 -0500

    Use userProvided at top level app

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { UserProvider } from './utils/UserContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 
 function App() {
-  const [auth, setAuth] = useState({})
-
-  useEffect(() => {
-    fetch('/user/current-session')
-      .then((response) => response.json())
-      .then((session) => setAuth(session.data))
-  }, [])
-
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <ProtectedRoute
-          exact
-          path="/dashboard"
-          component={Dashboard}
-          auth={auth}
-        />
-      </Switch>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </UserProvider>
   )
 }
 
