102e7cbb7efbed9180eec7ed6fb379c0f485b4b5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 04:38:49 -0500

    Uncomment func to login

--- client/src/utils/user-API.js
+++ client/src/utils/user-API.js
@@ -2,15 +2,15 @@ export function getSession() {
   return fetch('/user/current-session').then((response) => response.json())
 }
 
-// export function login(username, password) {
-//   return fetch('/user/login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ username, password }),
-//   }).then((response) => response.json())
-// }
+export function login(username, password) {
+  return fetch('/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  }).then((response) => response.json())
+}
 
 // export function logout() {
 //   return fetch('/user/logout').then((response) => response.json())
