8320d639c0bc80ca0cc63594b2ac5cc01da2c4c2
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 23:13:05 -0500

    Protect dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
@@ -18,7 +19,12 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <ProtectedRoute
+          exact
+          path="/dashboard"
+          component={Dashboard}
+          auth={auth}
+        />
       </Switch>
     </Router>
   )
