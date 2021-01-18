65e145b92d02294d964bb965702520cfb8b8b49d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-17 21:51:51 -0500

    Create page routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
