9fbb6fff736f25bac36020a2336bbebd3d849f83
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 00:16:17 -0500

    Create signup form

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function Signup() {
-  return <h1>Signup</h1>
+function Signup(props) {
+  const email = useRef('')
+  const username = useRef('')
+  const password = useRef('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email.current.value)
+    console.log(username.current.value)
+    console.log(password.current.value)
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="email">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          ref={email}
+          type="email"
+          placeholder="email@example.com"
+        />
+      </Form.Group>
+      <Form.Group controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control ref={username} type="text" placeholder="username" />
+      </Form.Group>
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control ref={password} type="password" />
+      </Form.Group>
+      <Button type="submit">Sign Up</Button>
+    </Form>
+  )
 }
 
 export default Signup
