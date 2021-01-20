77d207ed784d495047bcbedd1078b73d2170e591
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-19 22:10:21 -0500

    Add post routes

---
+++ routes/post-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router()
+const mongoose = require('mongoose')
+const { Post } = require('../models')
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find({})
+    res.status(200).send({ data: posts })
+  } catch (err) {
+    res.status(500).send({ err: err.message })
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body)
+    res.status(201).send({ data: newPost })
+  } catch (err) {
+    res.status(500).send({ err: err.message })
+  }
+})
+
+router.put('/', async (req, res) => {
+  try {
+    const _id = mongoose.Types.ObjectId(req.body._id)
+    const post = await Post.find({ _id })
+    if (post.length === 0) {
+      return res.status(404).send({ err: 'Post listing not found.' })
+    }
+    Post.findByIdAndUpdate(
+      req.body._id,
+      req.body,
+      { new: true },
+      (_, result) => {
+        res.status(200).send({ data: result })
+      }
+    )
+  } catch (err) {
+    res.status(500).send({ err: err.message })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const _id = mongoose.Types.ObjectId(req.params.id)
+    const post = await Post.find({
+      _id,
+    })
+    if (post.length === 0) {
+      return res.status(404).send({ err: 'Post listing not found.' })
+    }
+    await Post.deleteOne({ _id })
+    res.status(200).send({ data: post })
+  } catch (err) {
+    res.status(500).send({ err: err.message })
+  }
+})
+
+module.exports = router
