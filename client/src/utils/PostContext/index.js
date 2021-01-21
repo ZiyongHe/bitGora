67379a27611cf8ec4aa447ab326171bc6cc46991
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-21 13:00:33 -0500

    Add PostContext

---
+++ client/src/utils/PostContext/index.js
@@ -0,0 +1,36 @@
+import React, { useReducer, createContext, useContext } from 'react'
+import { SET_ALL_POSTS, SET_ERR } from './actions.js'
+
+const PostContext = createContext()
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_ALL_POSTS:
+      return { ...state, err: '', allPosts: action.allPosts }
+    case SET_ERR:
+      return { ...state, err: action.err }
+    default:
+      throw new Error(`Invalid Post dispatch action: ${action.type}`)
+  }
+}
+
+export function PostProvider(props) {
+  const [posts, dispatch] = useReducer(reducer, {
+    allPosts: [],
+    ownedPosts: [],
+    err: '',
+  })
+
+  return <PostContext.Provider value={[posts, dispatch]} {...props} />
+}
+
+export function usePost() {
+  const context = useContext(PostContext)
+  if (!context) {
+    throw new Error(
+      'usePost must be called from a descendent from PostProvider'
+    )
+  }
+
+  return context
+}
