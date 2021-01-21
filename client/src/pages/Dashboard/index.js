13e20874595ef6bddeb8820a0823f8dcceb3659b
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-21 11:32:12 -0500

    Create state to hold loading post data and console it out

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAllPosts } from '../../utils/post-API'
 
 function Dashboard() {
+  const [allPosts, setAllPosts] = useState({
+    posts: [],
+    err: '',
+  })
+
+  useEffect(() => {
+    getAllPosts()
+      .then((result) => {
+        if (result.err) {
+          setAllPosts((prevState) => ({
+            ...prevState,
+            err: result.err,
+          }))
+        } else {
+          setAllPosts({
+            posts: result.data,
+            err: '',
+          })
+        }
+      })
+      .catch(() => {
+        setAllPosts((prevState) => ({
+          ...prevState,
+          err: 'Something went wrong. Unable to load posts at this time.',
+        }))
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(allPosts)
+  }, [allPosts])
+
   return <h1>Dashboard</h1>
 }
 
