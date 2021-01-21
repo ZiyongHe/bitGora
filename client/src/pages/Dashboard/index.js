7a527bb8dfb3f0c6457193353594de4fe9d2aabc
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-21 13:00:58 -0500

    Lift up post state to post provider

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,42 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import PostCard from '../../components/PostCard'
+import { usePost } from '../../utils/PostContext'
+import { SET_ALL_POSTS, SET_ERR } from '../../utils/PostContext/actions'
 import { getAllPosts } from '../../utils/post-API'
 
 function Dashboard() {
-  const [allPosts, setAllPosts] = useState({
-    posts: [],
-    err: '',
-  })
+  const [posts, dispatch] = usePost()
 
   useEffect(() => {
     getAllPosts()
       .then((result) => {
         if (result.err) {
-          setAllPosts((prevState) => ({
-            ...prevState,
-            err: result.err,
-          }))
+          dispatch({ type: SET_ERR, err: result.err })
         } else {
-          setAllPosts({
-            posts: result.data,
-            err: '',
-          })
+          dispatch({ type: SET_ALL_POSTS, allPosts: result.data })
         }
       })
       .catch(() => {
-        setAllPosts((prevState) => ({
-          ...prevState,
-          err: 'Something went wrong. Unable to load posts at this time.',
-        }))
+        dispatch({
+          type: SET_ERR,
+          err: 'Something went wrong. Unable to load new posts at this time.',
+        })
       })
   }, [])
 
   useEffect(() => {
-    console.log(allPosts)
-  }, [allPosts])
+    console.log(posts)
+  }, [posts])
 
   return (
     <Container>
@@ -46,8 +39,8 @@ function Dashboard() {
         </Col>
       </Row>
       <Row>
-        {allPosts.posts.map((post) => (
-          <Col xs={12} md={6}>
+        {posts.allPosts.map((post) => (
+          <Col xs={12} md={6} key={post._id}>
             <PostCard post={post} editable={true} />
           </Col>
         ))}
