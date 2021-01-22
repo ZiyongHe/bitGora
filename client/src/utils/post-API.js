c856c5afb7504c37f460cb855232bbdb68f8af3a
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-22 00:14:50 -0500

    Add api func to get owned posts

--- client/src/utils/post-API.js
+++ client/src/utils/post-API.js
@@ -1,3 +1,7 @@
 export function getUnownedPosts() {
   return fetch('/post/unowned').then((response) => response.json())
 }
+
+export function getOwnedPosts() {
+  return fetch('/post/owned').then((response) => response.json())
+}
