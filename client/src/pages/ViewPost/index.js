e2f0330a7538b4dfa27a8f98d0f8819fe0edd9fe
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-22 01:03:44 -0500

    Add viewPost page

---
+++ client/src/pages/ViewPost/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { viewPost } from '../../utils/post-API'
+
+function ViewPost() {
+  const { id } = useParams()
+  const [post, setPost] = useState({
+    postData: {
+      userName: '',
+      userEmail: '',
+      name: '',
+      image: '',
+      description: '',
+      price: '',
+      createdAt: '',
+      sold: false,
+    },
+    isOwned: false,
+  })
+  const [err, setErr] = useState('')
+
+  useEffect(() => {
+    viewPost(id)
+      .then((response) => {
+        if (response.err) {
+          setErr(response.err)
+        } else {
+          setPost(response.data)
+        }
+      })
+      .catch(() => {
+        setErr('Something went wrong. Cannot view post data.')
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(post)
+  }, [post])
+
+  return <h1>{post.postData.name}</h1>
+}
+
+export default ViewPost
