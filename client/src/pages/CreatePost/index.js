a9050a90172fc8fc961b150f30c4080109ab0e19
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-22 17:51:10 -0500

    Add form to create post

--- client/src/pages/CreatePost/index.js
+++ client/src/pages/CreatePost/index.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function CreatePost() {
-  return <h1>Create a post</h1>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Create a post</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="My Amazing Product" />
+            </Form.Group>
+            <Form.Group controlId="image">
+              <Form.File label="Image of Product" />
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows={4} />
+            </Form.Group>
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Create Post
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default CreatePost
