84c96d81699be8a78ff057a99dfe6c3404d970f7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-22 14:03:16 -0500

    Move post provider out of App and into UserRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { UserProvider } from './utils/UserContext'
-import { PostProvider } from './utils/PostContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import PublicRoute from './components/PublicRoute'
 import MainNav from './components/MainNav'
@@ -15,22 +14,20 @@ import ChatRoom from './pages/Chatroom/ChatRoom'
 function App() {
   return (
     <UserProvider>
-      <PostProvider>
-        <Router>
-          <MainNav />
-          <Switch>
-            <PublicRoute exact path="/" component={Login} />
-            <PublicRoute exact path="/signup" component={Signup} />
-            <ProtectedRoute path="/user" component={UserRoutes} />
-            <ProtectedRoute exact path="/post/:id" component={ViewPost} />
+      <Router>
+        <MainNav />
+        <Switch>
+          <PublicRoute exact path="/" component={Login} />
+          <PublicRoute exact path="/signup" component={Signup} />
+          <ProtectedRoute path="/user" component={UserRoutes} />
+          <ProtectedRoute exact path="/post/:id" component={ViewPost} />
 
-            {/* Currently unused */}
-            <PublicRoute exact path="/home" component={Home} />
-            <PublicRoute exact path="/chatroom" component={Signup} />
-            <PublicRoute exact path="/:roomId" component={ChatRoom} />
-          </Switch>
-        </Router>
-      </PostProvider>
+          {/* Currently unused */}
+          <PublicRoute exact path="/home" component={Home} />
+          <PublicRoute exact path="/chatroom" component={Signup} />
+          <PublicRoute exact path="/:roomId" component={ChatRoom} />
+        </Switch>
+      </Router>
     </UserProvider>
   )
 }
--- client/src/routes/UserRoutes/index.js
+++ client/src/routes/UserRoutes/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PostProvider } from '../../utils/PostContext'
 import { Switch } from 'react-router-dom'
 import ProtectedRoute from '../../components/ProtectedRoute'
 import Dashboard from '../../pages/Dashboard'
@@ -7,18 +8,20 @@ import Profile from '../../pages/Profile'
 function UserRoutes({ match }) {
   console.log(match.path)
   return (
-    <Switch>
-      <ProtectedRoute
-        exact
-        path={`${match.path}/dashboard`}
-        component={Dashboard}
-      />
-      <ProtectedRoute
-        exact
-        path={`${match.path}/profile`}
-        component={Profile}
-      />
-    </Switch>
+    <PostProvider>
+      <Switch>
+        <ProtectedRoute
+          exact
+          path={`${match.path}/dashboard`}
+          component={Dashboard}
+        />
+        <ProtectedRoute
+          exact
+          path={`${match.path}/profile`}
+          component={Profile}
+        />
+      </Switch>
+    </PostProvider>
   )
 }
 
