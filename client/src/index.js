adc06b3ae2afe147c1c90f35f2e059b26b35595b
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-27 21:24:14 -0500

    Add index.css styles

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
